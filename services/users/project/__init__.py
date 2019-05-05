a246703318ff120deb69618eb0ec11a225d04d6a
Ran Ding <ran.ding@compass.com>
2019-05-05 14:09:53 -0400

    added flask debug toolbar

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,9 +1,12 @@
 import os
-from flask import Flask  # new
-from flask_sqlalchemy import SQLAlchemy
 
-# instantiate the db
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_debugtoolbar import DebugToolbarExtension  # new
+
+# instantiate the extensions
 db = SQLAlchemy()
+toolbar = DebugToolbarExtension()  # new
 
 
 def create_app(script_info=None):
@@ -16,6 +19,7 @@ def create_app(script_info=None):
 
     # set up extensions
     db.init_app(app)
+    toolbar.init_app(app)  # new
 
     # register blueprints
     from project.api.users import users_blueprint
