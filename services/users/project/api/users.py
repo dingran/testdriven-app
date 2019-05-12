7e6e831bb08710a593f621ddc8cc769bba747bac
Ran Ding <dingran.nju@gmail.com>
2019-05-12 14:33:21 -0400

    flake8 fix

--- services/users/project/api/users.py
+++ services/users/project/api/users.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, render_template, jsonify
+from flask import Blueprint, request, render_template
 from flask_restful import Resource, Api
 from sqlalchemy import exc
 
