a042fcd4a0e94f7b7bf271a1205a174846b0919e
Ran Ding <dingran.nju@gmail.com>
2019-05-12 20:13:54 -0400

    added auth

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -6,10 +6,12 @@ class BaseConfig:
     DEBUG = False  # new
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'my_precious'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     DEBUG_TB_ENABLED = False
     DEBUG_TB_INTERCEPT_REDIRECTS = False
-    BCRYPT_LOG_ROUNDS = 13  # new
+    BCRYPT_LOG_ROUNDS = 13
+    TOKEN_EXPIRATION_DAYS = 30  # new
+    TOKEN_EXPIRATION_SECONDS = 0  # new
 
 
 class DevelopmentConfig(BaseConfig):
@@ -24,6 +26,8 @@ class TestingConfig(BaseConfig):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
     BCRYPT_LOG_ROUNDS = 4  # new
+    TOKEN_EXPIRATION_DAYS = 0  # new
+    TOKEN_EXPIRATION_SECONDS = 3  # new
 
 
 class ProductionConfig(BaseConfig):
