1fee822f3ac2c9572fa5111b1c333f06ad237149
Ran Ding <dingran.nju@gmail.com>
2019-05-13 22:48:03 -0400

    react auth pt1

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -5,6 +5,7 @@ import UsersList from "./components/UsersList";
 import AddUser from "./components/AddUser";
 import Form from "./components/Form";
 import About from "./components/About";
+import Logout from "./components/Logout";
 import NavBar from "./components/NavBar";
 
 class App extends Component {
@@ -20,13 +21,19 @@ class App extends Component {
         email: "",
         password: "",
       },
+      isAuthenticated: false,
     };
     this.addUser = this.addUser.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleUserFormSubmit = this.handleUserFormSubmit.bind(this);
+    this.handleFormChange = this.handleFormChange.bind(this);
+    this.logoutUser = this.logoutUser.bind(this);
   }
+
   componentDidMount() {
     this.getUsers();
   }
+
   getUsers() {
     axios
       .get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
@@ -37,6 +44,7 @@ class App extends Component {
         console.log(err);
       });
   }
+
   addUser(event) {
     event.preventDefault();
     const data = {
@@ -53,11 +61,56 @@ class App extends Component {
         console.log(err);
       });
   }
+
   handleChange(event) {
     const obj = {};
     obj[event.target.name] = event.target.value;
     this.setState(obj);
   }
+
+  handleUserFormSubmit(event) {
+    event.preventDefault();
+    const formType = window.location.href.split("/").reverse()[0];
+    let data = {
+      email: this.state.formData.email,
+      password: this.state.formData.password,
+    };
+    if (formType === "register") {
+      data.username = this.state.formData.username;
+    }
+    const url = `${process.env.REACT_APP_USERS_SERVICE_URL}/auth/${formType}`;
+    axios
+      .post(url, data)
+      .then(res => {
+        this.clearFormState();
+        window.localStorage.setItem("authToken", res.data.auth_token);
+        this.setState({ isAuthenticated: true });
+        console.log(this.state);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  clearFormState() {
+    this.setState({
+      formData: { username: "", email: "", password: "" },
+      username: "",
+      email: "",
+    });
+  }
+
+  handleFormChange(event) {
+    const obj = this.state.formData;
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
+  }
+
+  logoutUser() {
+    window.localStorage.clear();
+    this.setState({ isAuthenticated: false });
+  }
+
   render() {
     return (
       <div>
@@ -70,11 +123,24 @@ class App extends Component {
                 <Switch>
                   <Route
                     exact
+                    path="/logout"
+                    render={() => (
+                      <Logout
+                        logoutUser={this.logoutUser}
+                        isAuthenticated={this.state.isAuthenticated}
+                      />
+                    )}
+                  />
+                  <Route
+                    exact
                     path="/register"
                     render={() => (
                       <Form
                         formType={"Register"}
                         formData={this.state.formData}
+                        handleUserFormSubmit={this.handleUserFormSubmit}
+                        handleFormChange={this.handleFormChange}
+                        isAuthenticated={this.state.isAuthenticated}
                       />
                     )}
                   />
@@ -82,7 +148,13 @@ class App extends Component {
                     exact
                     path="/login"
                     render={() => (
-                      <Form formType={"Login"} formData={this.state.formData} />
+                      <Form
+                        formType={"Login"}
+                        formData={this.state.formData}
+                        handleUserFormSubmit={this.handleUserFormSubmit}
+                        handleFormChange={this.handleFormChange}
+                        isAuthenticated={this.state.isAuthenticated}
+                      />
                     )}
                   />
                   <Route
